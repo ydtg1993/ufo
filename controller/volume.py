2da35bc3cafde33d7d585b6909db9c0f5d877d42
hikki <hikki@qqc88.net>
2023-08-21 17:56:38 +0800

    Changes

--- controller/volume.py
+++ controller/volume.py
@@ -8,7 +8,8 @@ from assiatant.proxy import Proxy
 from assiatant.rd import RedisConnector
 from configparser import ConfigParser
 from model.new_model import NewModel
-
+import re
+import json
 
 class Volume:
     def __init__(self, **kwargs):
@@ -46,10 +47,24 @@ class Volume:
                 main = WB.find_element(By.CLASS_NAME,'article-area')
                 exist = DB.session.query(NewModel).filter(NewModel.source_url==link).first()
                 if exist is None:
+                    desc = []
+                    banner = main.find_element(By.CSS_SELECTOR,"figure.article-span-photo img")
+                    desc.append({'type':'img','val':banner.get_attribute('src'),'alt':banner.get_attribute('alt')})
+                    paragraph = WB.find_elements(By.CLASS_NAME,"article-paragraph")
+                    for p in paragraph:
+                        childrenDom = p.get_attribute('innerHTML')
+                        if re.compile(r'<b>.*?</b>').search(childrenDom):
+                            desc.append({'type':'b','val':p.text})
+                        elif re.compile(r'<img').search(childrenDom):
+                            imgDom = p.find_element(By.TAG_NAME,'img')
+                            desc.append({'type': 'img', 'val': imgDom.get_attribute('data-src'),'alt':imgDom.get_attribute('alt')})
+                        else :
+                            desc.append({'type':'text','val':p.text})
+
                     new = NewModel(title=title,
                              full_title=main.find_element(By.CSS_SELECTOR,'.article-header h1').text,
                              source_url=link,
-                             introduce='',
+                             introduce=json.dumps(desc),
                              source_id=8,
                              category_id=1,)
                     DB.session.add(new)
