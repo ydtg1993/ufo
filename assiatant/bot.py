23c84c3216525f335a45e5ead768d205cc1eefd3
ydtg1993 <946818508@qq.com>
2023-08-23 00:10:43 +0800

    1

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -11,8 +11,7 @@ class Bot(object):
             options = uc.ChromeOptions()
             options.add_argument("window-size=1920,1080")
             options.add_argument('--blink-settings=imagesEnabled=false')
-            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-            options.add_argument(f'user-agent={user_agent}')
+            
             driver = uc.Chrome(options=options,headless=True,debug=True,browser_executable_path="/usr/bin/google-chrome")
             return driver
         except BaseException as e:
