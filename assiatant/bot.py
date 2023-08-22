6d101aeba30a78c6984c45b08ee39df6b8fe6748
ydtg1993 <946818508@qq.com>
2023-08-23 00:13:43 +0800

    1

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -9,10 +9,15 @@ class Bot(object):
     def start(self):
         try:
             options = uc.ChromeOptions()
+            options.add_argument("start-maximized")
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--headless')
             options.add_argument("window-size=1920,1080")
-            options.add_argument('--blink-settings=imagesEnabled=false')
-
-            driver = uc.Chrome(options=options,headless=True,debug=True,browser_executable_path="/usr/bin/google-chrome")
+            options.add_argument("--disable-gpu")
+            driver = uc.Chrome(options=options,browser_executable_path="/usr/bin/google-chrome")
+            driver.set_page_load_timeout(10)
+            driver.implicitly_wait(10)
             return driver
         except BaseException as e:
             print(f'webview开启失败{e}')
