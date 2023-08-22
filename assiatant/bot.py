a9f224dfd2434d30ef5938af46216243c9f71033
ydtg1993 <946818508@qq.com>
2023-08-23 00:20:43 +0800

    1

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -1,3 +1,4 @@
+import os
 from configparser import ConfigParser
 import undetected_chromedriver as uc
 
@@ -15,7 +16,8 @@ class Bot(object):
             options.add_argument('--headless')
             options.add_argument("window-size=1920,1080")
             options.add_argument("--disable-gpu")
-            driver = uc.Chrome(options=options,browser_executable_path="/usr/bin/google-chrome",driver_executable_path="./../chromedriver")
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            driver = uc.Chrome(options=options,browser_executable_path="/usr/bin/google-chrome",driver_executable_path=current_dir+"/../chromedriver")
             driver.set_page_load_timeout(10)
             driver.implicitly_wait(10)
             return driver
