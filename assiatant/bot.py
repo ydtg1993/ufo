745711d3109fef0b5b77d1d133695f39e07e6e61
ydtg1993 <946818508@qq.com>
2023-08-22 22:15:50 +0800

    1

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -1,4 +1,5 @@
 import threading
+import time
 from configparser import ConfigParser
 import undetected_chromedriver as uc
 
@@ -17,7 +18,8 @@ class Bot(object):
             options.add_argument('--no-sandbox')
             options.add_argument("window-size=1920,1080")
             options.add_argument('--blink-settings=imagesEnabled=false')
-            driver = uc.Chrome(options=options,executable_path="./../chromedriver")
+            driver = uc.Chrome(options=options,executable_path="./chromedriver")
+            time.sleep(5)
             with self.pool_lock:
                 self.pool.append(driver)
         except BaseException as e:
