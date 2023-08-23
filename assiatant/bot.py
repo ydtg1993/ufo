af343056675fa5055c534a5f2dc3a733f5be53d5
hikki <hikki@qqc88.net>
2023-08-23 09:19:49 +0800

    Changes

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -9,15 +9,17 @@ class Bot(object):
     def start(self):
         try:
             options = uc.ChromeOptions()
-            options.add_argument("start-maximized")
+            options.add_argument('--incognito')
+            options.add_argument("--start-maximized")
+            options.add_argument("--window-size=1920,1080")
             options.add_argument('--no-sandbox')
-            options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--headless')
-            options.add_argument("window-size=1920,1080")
-            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-extensions")
+            options.add_argument('--disable-application-cache')
+            options.add_argument('--disable-gpu')
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-setuid-sandbox")
             driver = uc.Chrome(options=options,browser_executable_path="/usr/bin/google-chrome")
-            driver.set_page_load_timeout(10)
-            driver.implicitly_wait(10)
             return driver
         except BaseException as e:
             print(f'webview开启失败{e}')
