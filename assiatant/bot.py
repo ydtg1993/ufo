f786b267e066143aa539ba1cabb56529eaf98295
hikki <hikki@qqc88.net>
2023-08-23 11:13:48 +0800

    Changes

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -4,21 +4,21 @@ import undetected_chromedriver as uc
 
 class Bot(object):
     def __init__(self, config: ConfigParser):
-        self.DEBUG = bool(config.get("App", "DEBUG"))
+        self.DEBUG = True if config.get("App", "DEBUG") == 'on' else False
 
     def start(self):
         try:
             options = uc.ChromeOptions()
-            #if not self.DEBUG:
-            options.add_argument("--blink-settings=imagesEnabled=false")
-            options.add_argument("--headless")
-            options.add_argument("--disable-gpu")
-            options.add_argument('--no-sandbox')
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--disable-web-security")
-            options.add_argument("--disable-extensions")
-            options.add_argument('--disable-application-cache')
-            options.add_argument("--disable-setuid-sandbox")
+            if not self.DEBUG:
+                options.add_argument("--blink-settings=imagesEnabled=false")
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument('--no-sandbox')
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-web-security")
+                options.add_argument("--disable-extensions")
+                options.add_argument('--disable-application-cache')
+                options.add_argument("--disable-setuid-sandbox")
 
             driver = uc.Chrome(options=options)
             return driver
