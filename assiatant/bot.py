26115950ac6810da6d75705fa88815377888cc2c
hikki <hikki@qqc88.net>
2023-08-23 09:55:34 +0800

    Changes

--- assiatant/bot.py
+++ assiatant/bot.py
@@ -4,20 +4,22 @@ import undetected_chromedriver as uc
 
 class Bot(object):
     def __init__(self, config: ConfigParser):
-        num = int(config.get("Bot", "thread"))
+        self.DEBUG = int(config.get("App", "DEBUG"))
 
     def start(self):
         try:
             options = uc.ChromeOptions()
-            options.add_argument("--blink-settings=imagesEnabled=false")
-            options.add_argument("--headless")
-            options.add_argument("--disable-gpu")
-            options.add_argument('--no-sandbox')
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--disable-web-security")
-            options.add_argument("--disable-extensions")
-            options.add_argument('--disable-application-cache')
-            options.add_argument("--disable-setuid-sandbox")
+            if not self.DEBUG:
+                options.add_argument("--blink-settings=imagesEnabled=false")
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument('--no-sandbox')
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-web-security")
+                options.add_argument("--disable-extensions")
+                options.add_argument('--disable-application-cache')
+                options.add_argument("--disable-setuid-sandbox")
+
             driver = uc.Chrome(options=options,browser_executable_path="/usr/bin/google-chrome")
             return driver
         except BaseException as e:
