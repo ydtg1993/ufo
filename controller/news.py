c6ab3e52d6d4dda47723bd374a083a3b3e41118f
hikki <hikki@qqc88.net>
2023-09-05 17:18:56 +0800

    Changes

--- controller/news.py
+++ controller/news.py
@@ -5,12 +5,12 @@ from controller.reuter import Reuter
 
 class News:
     def __init__(self):
-        ReuterThreads = []
-        for _ in range(2):
+        threads = []
+        for _ in range(1):
             thread = threading.Thread(target=Reuter)
-            ReuterThreads.append(thread)
+            threads.append(thread)
             thread.start()
 
-        for thread in ReuterThreads:
+        for thread in threads:
             thread.join()
 
