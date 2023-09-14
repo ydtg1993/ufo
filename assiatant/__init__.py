450c7b2774c92ce621592738fb6d11f290f52c2b
hikki <hikki@qqc88.net>
2023-09-14 13:27:31 +0800

    Changes

---
+++ assiatant/__init__.py
@@ -0,0 +1,13 @@
+import configparser
+from assiatant.globe import GB
+from assiatant.bot import Bot
+from assiatant.db import MysqlConnector
+from assiatant.rd import RedisConnector
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+GB["config"] = config
+GB["mysql"] = MysqlConnector(config)
+GB["redis"] = RedisConnector(config)
+GB["bot"] = Bot(config)
+GB["bot"].start_pool(int(config.get("Bot", "MAX_THREADS")))
--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import configparser
 import time
 import schedule
-from assiatant.bot import Bot
-from assiatant.db import MysqlConnector
-from assiatant.globe import GB
-from assiatant.rd import RedisConnector
 from controller.news import News
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    GB["config"] = config
-    GB["mysql"] = MysqlConnector(config)
-    GB["redis"] = RedisConnector(config)
-    GB["bot"] = Bot(config)
-    GB["bot"].start_pool(int(config.get("Bot", "MAX_THREADS")))
     News()
-
     schedule.every(1).hours.do(News)
     while True:
         schedule.run_pending()
