81363bfecfc22dac8a5becf2425145d4be03de42
ydtg1993 <946818508@qq.com>
2023-10-12 23:30:12 +0800

    1

--- assiatant/db.py
+++ assiatant/db.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 from configparser import ConfigParser
 
+
 class MysqlConnector:
+    session = None
+
     def __init__(self, config: ConfigParser):
         self.DB_HOST = config.get("Database", "HOST")
         self.DB_PORT = int(config.get("Database", "PORT"))
@@ -13,21 +17,26 @@ class MysqlConnector:
             self.engine = create_engine(
                 f"mysql+pymysql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}",
                 pool_size=20, max_overflow=10)
-            self.engine.connect()
-            session = sessionmaker(bind=self.engine)
-            self.session = session()
+            self.connect()
         except BaseException as e:
             print(f'{e}')
         else:
             print('mysql链接成功')
 
+    def connect(self):
+        try:
+            self.engine.connect()
+            session = sessionmaker(bind=self.engine)
+            self.session = session()
+            print('MySQL connection successful')
+        except OperationalError as e:
+            print(f'Error connecting to MySQL: {e}')
+
     def reconnect(self):
         try:
             self.session.close()
             self.engine.dispose()
-            self.engine.connect()
-            session = sessionmaker(bind=self.engine)
-            self.session = session()
+            self.connect()
         except BaseException as e:
             print(f'{e}')
         else:
