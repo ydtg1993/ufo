1560a6b1addf69307290ab346d108b44789b382c
ydtg1993 <946818508@qq.com>
2023-10-12 22:49:26 +0800

    1

--- controller/news.py
+++ controller/news.py
@@ -1,11 +1,11 @@
-from assiatant.globe import GB
+from assiatant import GB
 from controller.nytime import Nytime
 from controller.reuter import Reuter
 
 
 class News:
     def __init__(self):
-        if GB["config"].get('App', 'PROJECT') == 'nytime':
+        if GB.config.get('App', 'PROJECT') == 'nytime':
             Nytime()
         else:
             Reuter()
