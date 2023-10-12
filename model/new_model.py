31ed070d009ec2733d60bb8fee757d63008bdafb
ydtg1993 <946818508@qq.com>
2023-10-12 23:26:54 +0800

    1

--- model/new_model.py
+++ model/new_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from assiatant import GB
 
 Base = declarative_base()
 
