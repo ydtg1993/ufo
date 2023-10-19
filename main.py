2e95202a89ad06e3f70f87dc1cf39244a5f9151b
ydtg1993 <946818508@qq.com>
2023-10-19 22:49:27 +0800

    1

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ import logging
 import random
 import time
 import schedule
-
 from assiatant import GB
 from controller.comic import Comic
 from controller.img import Img
 from controller.menu import Menu
+from model.source_chapter_model import SourceChapterModel
 from model.source_comic_model import SourceComicModel
 
 
 def main():
-    process_update_comic()
-    return
     schedule.every(15).minutes.do(process_menu)
     schedule.every(3).minutes.do(process_comic).tag('enabled')
     schedule.every(2).minutes.do(process_img).tag('enabled')
     schedule.every(45).minutes.do(process_update_comic).tag('enabled')
     schedule.every(3).hours.do(reset_comic_update_queue).tag('enabled')
-
+    schedule.every(12).hours.do(reset_chapter_img_queue).tag('enabled')
     while True:
         schedule.run_pending()
         time.sleep(5)
@@ -52,23 +50,7 @@ def process_img():
 
 def process_update_comic():
     try:
-        batch_size = 500
-        offset = 0
-        tasks = GB.redis.get_queue(GB.config.get("App", "PROJECT") + ":chapter:task", 0, -1)
-        while True:
-            # 查询并获取下一批结果
-            results = GB.mysql.session.query(SourceComicModel.id).filter(
-                SourceComicModel.source_chapter_count != SourceComicModel.chapter_count).offset(offset).limit(
-                batch_size).all()
-            for result in results:
-                tasks.append(str(result[0]))
-            tasks = list(set(tasks))
-            GB.redis.delete(GB.config.get("App", "PROJECT") + ":chapter:task")
-            for _, comic_id in enumerate(tasks):
-                GB.redis.enqueue(GB.config.get("App", "PROJECT") + ":chapter:task", comic_id)
-            offset += batch_size
-            if len(results) < batch_size:
-                break
+        Comic(True)
     except Exception as e:
         logger = logging.getLogger(__name__)
         logger.exception(str(e))
@@ -76,7 +58,45 @@ def process_update_comic():
 
 def reset_comic_update_queue():
     try:
-        Comic(True)
+        batch_size = 500
+        offset = 0
+        tasks = GB.redis.get_queue(GB.config.get("App", "PROJECT") + ":chapter:task", 0, -1)
+        while True:
+            results = GB.mysql.session.query(SourceComicModel.id).filter(
+                SourceComicModel.source_chapter_count != SourceComicModel.chapter_count).offset(offset).limit(
+                batch_size).all()
+            for result in results:
+                tasks.append(str(result[0]))
+            tasks = list(set(tasks))
+            offset += batch_size
+            if len(results) < batch_size:
+                GB.redis.delete(GB.config.get("App", "PROJECT") + ":chapter:task")
+                for _, comic_id in enumerate(tasks):
+                    GB.redis.enqueue(GB.config.get("App", "PROJECT") + ":chapter:task", comic_id)
+                break
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.exception(str(e))
+
+
+def reset_chapter_img_queue():
+    try:
+        batch_size = 500
+        offset = 0
+        tasks = GB.redis.get_queue(GB.config.get("App", "PROJECT") + ":img:task", 0, -1)
+        while True:
+            results = GB.mysql.session.query(SourceChapterModel.id).filter(
+                SourceChapterModel.img_count == 0).offset(offset).limit(
+                batch_size).all()
+            for result in results:
+                tasks.append(str(result[0]))
+            tasks = list(set(tasks))
+            offset += batch_size
+            if len(results) < batch_size:
+                GB.redis.delete(GB.config.get("App", "PROJECT") + ":img:task")
+                for _, chapter_id in enumerate(tasks):
+                    GB.redis.enqueue(GB.config.get("App", "PROJECT") + ":img:task", chapter_id)
+                break
     except Exception as e:
         logger = logging.getLogger(__name__)
         logger.exception(str(e))
