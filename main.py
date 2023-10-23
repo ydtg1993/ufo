89c65521ee35f1a1724b4a43f9e9047042726818
ydtg1993 <946818508@qq.com>
2023-10-24 00:31:14 +0800

    1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 import random
 import threading
 import time
+
+from assiatant import GB
 from controller.comic import Comic
 from controller.img import Img
 from controller.menu import Menu
@@ -22,6 +24,7 @@ def main():
     fill_task(process_comic, 120)
     fill_task(process_img, 30)
     fill_task(process_update_comic)
+    fill_task(reset_comic_update_queue)
 
     for _, t in enumerate(tt):
         t.join()
@@ -49,7 +52,7 @@ def process_menu():
 def process_comic():
     while True:
         try:
-            i.insert_process('漫画详情页', datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 420)
+            i.insert_process('漫画详情页', datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 1800)
             Comic()
         except Exception as e:
             logger = logging.getLogger(__name__)
@@ -60,7 +63,7 @@ def process_comic():
 def process_img():
     while True:
         try:
-            i.insert_process('章节图片页', datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 420)
+            i.insert_process('章节图片页', datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 1800)
             Img()
         except Exception as e:
             logger = logging.getLogger(__name__)
@@ -78,6 +81,31 @@ def process_update_comic():
             logger.exception(str(e))
         time.sleep(random.randint(600, 1800))
 
+
+def reset_comic_update_queue():
+    while True:
+        try:
+            i.insert_process('重置漫画更新队列', datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 3600 * 9)
+            batch_size = 500
+            offset = 0
+            tasks = GB.redis.get_queue(GB.process_cache_conf['chapter']['key'], 0, -1)
+            while True:
+                results = GB.mysql['main'].session.query(SourceComicModel.id).filter(
+                    SourceComicModel.source_chapter_count != SourceComicModel.chapter_count).offset(offset).limit(
+                    batch_size).all()
+                for result in results:
+                    tasks.append(str(result[0]))
+                tasks = list(set(tasks))
+                offset += batch_size
+                if len(results) < batch_size:
+                    GB.redis.delete(GB.process_cache_conf['chapter']['key'])
+                    for _, comic_id in enumerate(tasks):
+                        GB.redis.enqueue(GB.process_cache_conf['chapter']['key'], comic_id)
+                    break
+        except Exception as e:
+            logger = logging.getLogger(__name__)
+            logger.exception(str(e))
+        time.sleep(3600 * 24)
 
 if __name__ == '__main__':
     main()
