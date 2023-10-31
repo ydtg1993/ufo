c4bd1c29d94168d22d77c20eec84644697de5b2e
ydtg1993 <946818508@qq.com>
2023-10-31 15:32:29 +0800

    1

--- controller/menu.py
+++ controller/menu.py
@@ -8,10 +8,14 @@ from assiatant import GB
 
 class Menu:
     def __init__(self):
-        wb = GB.bot.start(GB.config.get("App", "URL") + 'vod/show/by/hits/id/1/')
-        wb.find_element(By.CSS_SELECTOR, 'div.channel>div.mb_none a:last-child').click()
+        wb = GB.bot.start(GB.config.get("App", "URL"))
+        page = 179
         while True:
-            time.sleep(random.randint(5, 10))
+            if page <= 0:
+                break
+            url = GB.config.get("App", "URL") + 'vod/show/by/hits/id/{page}/'.format(page=page)
+            wb.get(url)
+            page -= 1
             boxes = wb.find_elements(By.CSS_SELECTOR, 'div.list>ul li')
             for _, box in enumerate(boxes):
                 cover = box.find_element(By.TAG_NAME, 'img').get_attribute('src')
@@ -23,7 +27,5 @@ class Menu:
                 GB.redis.enqueue(GB.process_cache_conf['detail']['key'],
                                  json.dumps({"title": title, "link": link, "cover": cover}))
                 GB.redis.set_hash(GB.process_cache_conf['av.unique']['key'], link, "0")
-            if wb.current_url == GB.config.get("App", "URL") + 'vod/show/by/hits/id/1/':
-                break
-            wb.find_elements(By.CSS_SELECTOR, 'div.channel>div.mb_none a.page_link')[1].click()
+            time.sleep(random.randint(3, 5))
         wb.quit()
