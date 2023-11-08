b291657544c0c4332894b2db91a909dcd0013473
ydtg1993 <946818508@qq.com>
2023-11-09 00:42:52 +0800

    bz

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#docker run -ti --name= -v /home/server/www/py-jav:/home/jav -p 8992:8992 --link myredis --link mydb --network= allonvendia/ufo:v1.0 /bin/sh
 python3 main.py > output.log 2>&1 &
