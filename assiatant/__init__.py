cac7ed7ecfc19bc70e04407bd79c610d4e99046d
ydtg1993 <946818508@qq.com>
2023-11-08 19:50:42 +0800

    1

--- assiatant/__init__.py
+++ assiatant/__init__.py
@@ -6,7 +6,6 @@ from assiatant.db import MysqlConnector
 from assiatant.rd import RedisConnector
 import logging.handlers
 
-ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # timezone setting
 os.environ["TZ"] = "Asia/Shanghai"
 
